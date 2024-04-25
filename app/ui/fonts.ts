b39005b7279206d8cf04446f033d1cd8e5c2bbf4
enimo <enimong@gmail.com>
2024-04-25 17:15:35 +0800

    upd add db

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,14 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Noto_Sans_SC } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
+
+export const NotoSansSC = Noto_Sans_SC({ 
+  weight: ['400', '700'],
+  subsets: ['latin'] 
+});
  
 export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
+
