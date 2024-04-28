4e4a0b83894b384cb0759a6c75dabedf189ca5b4
enimo <enimong@gmail.com>
2024-04-29 00:38:25 +0800

    support suspend component and chunk render

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // ppr: true, // 自动将Suspend组件包含的内容 切分为前端渲染
+  },
+}
 
 module.exports = nextConfig;
